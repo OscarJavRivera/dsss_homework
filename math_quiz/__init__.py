0a882c816dd6b8986e0b0737df2500bc7961b29c
Oscar Rivera <ing.oscar.rivera@gmail.com>
2024-11-08 23:51:33 +0100

    Init.py file was added for imports

---
+++ math_quiz/__init__.py
@@ -0,0 +1,6 @@
+# math_quiz/__init__.py
+
+from .math_quiz import generateRandomInteger, getRandomOperator, calculateExpression
+
+# Optionally, you can specify what should be available when importing * from this package
+__all__ = ['generateRandomInteger', 'getRandomOperator', 'calculateExpression']
